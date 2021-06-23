619d8c1501ae9c244425c234188ccdfdd4fe2ea8
Subwaytime <leon.l@nophase.de>
2021-06-23 15:31:11 +0200

    Update Types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,17 @@
 import { defineComponent, VNodeProps } from 'vue';
 
 export interface MountOptions {
+	component: Component;
+	children?: ChildComponent[]
 	props: VNodeProps;
-	children?: Component | Component[];
 	target?: string;
 };
+
+export interface ChildComponent {
+	component: Component;
+	props: VNodeProps;
+	target?: string;
+	slot?: string;
+}
 
 export type Component = ReturnType<typeof defineComponent>;
