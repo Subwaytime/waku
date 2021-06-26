529c86bcdef6f071f8003eb45eb599f80732e2b3
Subwaytime <leon.l@nophase.de>
2021-06-26 14:27:35 +0200

    Update Utility Functions
    Add isVueComponent

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,21 @@
+import type { Component } from './types';
+
 /**
  *
  * @param {*} v
  */
 
-export function isPlainObject(v) {
+export function isPlainObject(v: any) {
 	return !!v && typeof v === 'object' && (v.__proto__ === null || v.__proto__ === Object.prototype);
+}
+
+/**
+ *
+ * @param v
+ */
+
+export function isVueComponent(v: Component) {
+	return isPlainObject(v) && v.render && v.__file && v.__hmrId;
 }
 
 /**
@@ -13,15 +24,11 @@ export function isPlainObject(v) {
  */
 
 export function empty(value: any) {
-	if (value === null || value === undefined || value === '{}') {
+	if (value === null || value === undefined || value === '{}' || value === '') {
 		return true;
 	}
 
 	if (Array.isArray(value) && Object.keys(value).length <= 0) {
-		return true;
-	}
-
-	if (Reflect.ownKeys(value).length === 0 && value.constructor === Object) {
 		return true;
 	}
 
