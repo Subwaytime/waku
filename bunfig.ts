11c1d71538f438892c5b0f3fa09baed5d35d954e
Subwaytime <leon.l@nophase.de>
2024-10-11 02:09:46 +0200

    remove bun-plugin-dts and eslint

--- bunfig.ts
+++ bunfig.ts
@@ -1,12 +1,9 @@
-import dts from 'bun-plugin-dts';
-
 export default await Bun.build({
 	entrypoints: ['./src/index.ts'],
 	outdir: './dist',
 	minify: true,
 	external: ['vue'],
 	format: 'esm',
-	plugins: [dts()],
 });
 
 console.log('Build successful! ✅');
