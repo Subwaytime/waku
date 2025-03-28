13252bebe6a990d025a4d8731c1938155e4ac59d
Subwaytime <leon.l@nophase.de>
2025-03-28 22:47:30 +0100

    remove bunfig (doesn't support dts atm)

--- bunfig.ts
+++
@@ -1,9 +0,0 @@
-export default await Bun.build({
-	entrypoints: ['./src/index.ts'],
-	outdir: './dist',
-	minify: true,
-	external: ['vue'],
-	format: 'esm',
-});
-
-console.log('Build successful! ✅');
