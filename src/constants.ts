5c6897456a797f69421a4ea2466939463b17ca0e
Subwaytime <leon.l@nophase.de>
2025-04-24 17:30:56 +0200

    remove defaultOptions from constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,2 @@
 export const MODULE_NAME = 'waku';
 export const WakuSymbol: symbol = Symbol(MODULE_NAME);
-
-/**
- *
- */
-export const defaultOptions = {
-	inheritAttrs: false,
-	immediate: true,
-};
