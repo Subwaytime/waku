e874f2a5c61d303b246584fd05555c85384326bd
Subwaytime <leon.l@nophase.de>
2025-04-28 21:15:53 +0200

    include unplugin-vue to build mount component

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'tsdown';
 import dts from 'vite-plugin-dts';
+import vue from 'unplugin-vue/rolldown';
 
 export default defineConfig({
     clean: true,
@@ -10,6 +11,7 @@ export default defineConfig({
     outDir: 'dist',
     target: 'esnext',
     plugins: [
+        vue(),
         // FIX: Move this to `dts: true` once the output is similiar to the vite-plugin-dts
         // Note: tsdown+dts builds take around ~60ms, tsdown+vite-plugin-dts ~1060ms and vite+vite-plugin-dts ~1120ms
         dts({
