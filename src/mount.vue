601d494a6283278589ad104874d5fc0b0a845ed2
Subwaytime <leon.l@nophase.de>
2025-04-28 22:38:34 +0200

    add mount component

---
+++ src/mount.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="waku-mount">
+        <component
+            v-for="item in items"
+            :key="item.id"
+            :is="item.vNode"
+            v-memo="[item.id]"
+            v-bind="item.props"
+            v-on="item.emits"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+// import { type ComponentInstance, onMounted, onUnmounted } from 'vue';
+import { useWaku } from './core';
+
+const { items } = useWaku();
+</script>
+
+<style scoped>
+.waku-mount {
+    display: contents;
+}
+</style>
