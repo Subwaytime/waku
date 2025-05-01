9bb76fbb8ad661cfd92db7447fb9e9ec19e2443a
Subwaytime <leon.l@nophase.de>
2025-05-01 16:08:34 +0200

    update createWaku, include new root, add internal, update items to reactive

--- src/createWaku.ts
+++ src/createWaku.ts
@@ -1,6 +1,8 @@
-import type { App } from 'vue';
-import { type Waku, type WakuItem, setActiveWaku } from './core';
+import { type App, DefineComponent, Fragment, h, reactive } from 'vue';
+import { type Waku, type WakuItem, type WakuEvents, WakuEventsEnum, setActiveWaku } from './core';
 import { MODULE_NAME, WakuSymbol } from './constants';
+import WakuMount from './mount.vue';
+import mitt from 'mitt';
 
 export function createWaku(): Waku {
 	// biome-ignore lint/suspicious/noImplicitAnyLet:
@@ -9,12 +11,23 @@ export function createWaku(): Waku {
 	const waku: Waku = {
 		id: `${MODULE_NAME}-plugin`,
 		instance,
-		items: [],
+		items: reactive([]),
+		internal: mitt<WakuEvents>(),
 		install: (app: App) => {
 			setActiveWaku(waku);
 			app.provide(WakuSymbol, waku);
 			app.config.globalProperties.$waku = waku;
 			waku.instance = app;
+
+			const rootComponent = app._component as DefineComponent;
+			const originalRender = rootComponent.render;
+			rootComponent.render = function (...args: any) {
+				const root = originalRender?.apply(this, args);
+				return h(Fragment, null, [
+					h(root),
+					h(WakuMount)
+				]);
+			};
 		},
 		addItem(item: WakuItem) {
 			if (waku.getItem(item.id)) {
@@ -22,7 +35,11 @@ export function createWaku(): Waku {
 			}
 
 			waku.items?.push(item);
-			waku.instance?._container.appendChild(item.el);
+			waku.internal.emit(WakuEventsEnum.ITEM_ADDED, item.id);
+		},
+		removeItem(id: string) {
+			waku.items = waku.items?.filter((i) => i.id !== id);
+			waku.internal.emit(WakuEventsEnum.ITEM_REMOVED, id);
 		},
 		getItem(id: string) {
 			const item = waku.items?.find((i) => i.id === id);
@@ -31,10 +48,7 @@ export function createWaku(): Waku {
 				return false;
 			}
 			return item;
-		},
-		removeItem(id: string) {
-			waku.items = waku.items?.filter((i) => i.id !== id);
-		},
+		}
 	};
 
 	return waku;
