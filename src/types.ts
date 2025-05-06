c5c2ad2e640391af4da5fd298f5a039e525923f6
Subwaytime <leon.l@nophase.de>
2025-05-06 15:10:06 +0200

    update types

--- src/types.ts
+++ src/types.ts
@@ -1,73 +1,85 @@
 import type {
-	AllowedComponentProps,
-	Component,
 	RendererElement,
 	TeleportProps,
+	TransitionGroupProps,
+	TransitionProps,
 	VNode,
-	VNodeProps,
 } from 'vue';
 
 import type { ComponentProps, ComponentSlots } from 'vue-component-type-helpers';
 import type { SimplifyDeep } from 'type-fest';
 
-// DISABLED FOR NOW
-// type InternalProps =
-// 	| keyof VNodeProps
-// 	| keyof AllowedComponentProps
-// 	| keyof DefaultProps;
-// export type ExtractComponentProps<TComponent> = TComponent extends new () => {
-// 	$props: infer P;
-// }
-// 	? P
-// 	: never;
-// type ComponentProps<C extends Component> = C extends new (
-// 	...args: any
-// ) => any
-// 	? {
-// 			[K in keyof InstanceType<C> as K extends InternalProps
-// 				? never
-// 				: K]: InstanceType<C>;
-// 		}
-// 	: never;
-
-type Emits = {
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	[key: `on${Capitalize<string>}`]: (...args: any[]) => void;
-};
-
-export type Options<C> = {
-	component: C;
-	props?: ComponentProps<C>
-	emits?: Emits;
-	slots?: SlottedComponent<C, any>[];
-	target?: TeleportProps['to'];
-	// transition?: TransitionProps | TransitionGroupProps
-	immediate?: boolean;
-	inheritAttrs: boolean;
-};
-
 type RemoveIndexSignature<T> = {
 	[K in keyof T as K extends `${infer _}` ? K : never]: T[K]
 };
+
+type ReadonlyKeys<T> = {
+	[K in keyof T]-?: IfEquals<
+		{ [Q in K]: T[K] },
+		{ -readonly [Q in K]: T[K] },
+		never,
+		K
+	>
+}[keyof T];
+
+type IfEquals<X, Y, A = X, B = never> = (
+	(<T>() => T extends X ? 1 : 2) extends
+	(<T>() => T extends Y ? 1 : 2) ? A : B
+);
+
+type OnlyReadonlyNonOnProps<T> = {
+	[K in keyof T as K extends ReadonlyKeys<T>
+	? K extends `on${string}`
+	? never
+	: K
+	: never]: T[K];
+};
+
+type OnlyReadonlyOnProps<T> = {
+	[K in keyof T as K extends ReadonlyKeys<T>
+	? K extends `on${string}`
+	? K
+	: never
+	: never]: T[K];
+};
+
 export type SlotNames<C> = keyof RemoveIndexSignature<SimplifyDeep<ComponentSlots<C>>>;
-export type SlottedComponent<P, SC extends Component> = {
-	slotName: SlotNames<P>;
-	component: SC;
-	props?: ComponentProps<SC>;
-	emits?: Emits;
-	slots?: SlottedComponent<SC, any>[];
+
+type BaseProps<T> = OnlyReadonlyNonOnProps<ComponentProps<T>>;
+type BaseEmits<T> = OnlyReadonlyOnProps<ComponentProps<T>>;
+
+type BaseSlots<T> = {
+	[K in SlotNames<T>]?: <C>(input: BaseOptions<C>) => void | any;
+};
+
+type InferResolvedProps<P> = P extends object
+	? SimplifyDeep<BaseProps<P>>
+	: never;
+
+type InferResolvedEmits<E> = E extends object
+	? SimplifyDeep<BaseEmits<E>>
+	: never;
+
+export interface BaseOptions<C> {
+	component?: C;
+	props?: InferResolvedProps<C>;
+	emits?: InferResolvedEmits<C>;
+	target?: TeleportProps['to'];
+	slots?: BaseSlots<C>;
+	inheritAttrs?: boolean;
+	immediate?: boolean;
+	transition?: TransitionProps | TransitionGroupProps
 };
 
 export interface DefaultProps {
-	'data-mounted-id'?: string;
-	'data-is-programmatic'?: boolean;
+	'waku-mounted-id': string;
+	'waku-is-programmatic': boolean;
 	onDestroy: () => void;
-}
+};
 
-export type MountOptions<C> = { component: C } | Options<C>;
 export interface MountedComponentInstance {
 	id: string;
 	vNode: VNode;
-	el: RendererElement | Element | null;
+	el?: RendererElement | Element | null;
 	destroy: () => void;
 }
