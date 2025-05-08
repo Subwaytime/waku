5d86c621155bf2316826a3acc4a34d2515d0432d
Subwaytime <leon.l@nophase.de>
2025-05-08 23:27:47 +0200

    revert prepareData implementation (would cause double vnode creation)

--- src/actions/createSlot.ts
+++ src/actions/createSlot.ts
@@ -1,31 +1,70 @@
 import {
+    createVNode,
+    readonly,
+    mergeProps,
+    h,
+    type DefineComponent,
+    type Component,
     type VNode,
+    Teleport,
 } from 'vue';
-import { prepareData } from '~/actions/prepareData';
+import { defu } from 'defu';
 
-declare const _createSlotHandled: unique symbol;
+import { generateID } from '~/utils';
+import { handleSlots } from '~/actions/handleSlots';
+import { useWaku } from '~/core';
+import { MODULE_NAME } from '~/constants';
+
+// declare const _createSlotHandled: unique symbol;
 
 export interface CreatedSlot {
     id: string;
-    vNode: VNode;
-    [_createSlotHandled]: boolean;
+    component: Component;
+    data: any;
+    slots: any;
+    // [_createSlotHandled]: boolean;
 };
 
 export function createSlot<C>(input: any): CreatedSlot {
+    const waku = useWaku();
+
+    if (!waku.instance) {
+        throw new Error(`[${MODULE_NAME}]: No Vue Instance for Waku was provided!`);
+    }
+
+    const wrappedOptions = 'component' in input
+        ? input
+        : { component: input };
+
+    const id: string = generateID();
+
     const defaultOptions = {
         inheritAttrs: false,
         immediate: true,
     } as const;
 
-    const defaultProps= (id: string) => ({
+    const defaultProps = {
         'waku-slot-id': id,
-    }) as const;
+    } as const;
 
-    const { id, vNode } = prepareData(input, defaultProps, defaultOptions);
+    const opt = defu({}, { ...defaultOptions, ...wrappedOptions }) as any;
+    const component = opt.component as DefineComponent;
+
+    const data = readonly(
+        mergeProps(defaultProps, {
+            ...opt.props ?? {},
+            ...opt?.emits ?? {}
+        })
+    );
+
+    component.inheritAttrs = opt.inheritAttrs;
+    const slots = opt.slots ? handleSlots(opt.slots) : undefined;
 
     return {
         id,
-        vNode,
-        [_createSlotHandled]: true
+        component,
+        data,
+        slots
+        // [_createSlotHandled]: true
     }
 }
--- src/actions/mountComponent.ts
+++ src/actions/mountComponent.ts
@@ -1,3 +1,13 @@
+import {
+	createVNode,
+	readonly,
+	mergeProps,
+	h,
+	type DefineComponent,
+	Teleport,
+} from 'vue';
+import { defu } from 'defu';
+
 import { useWaku } from '~/core';
 import { unmountComponent } from '~/actions/unmountComponent';
 import { MODULE_NAME } from '~/constants';
@@ -5,8 +15,10 @@ import type {
 	DefaultProps,
 	MountedComponentInstance,
 	Options,
- } from '~/types';
-import { prepareData } from '~/actions/prepareData';
+} from '~/types';
+import { generateID } from '~/utils';
+import { handleSlots } from '~/actions/handleSlots';
+
 
 export function mountComponent<C>(input: Options<C>): MountedComponentInstance {
 	const waku = useWaku();
@@ -26,7 +38,32 @@ export function mountComponent<C>(input: Options<C>): MountedComponentInstance {
 		onDestroy: () => unmountComponent(id)
 	}) as const satisfies DefaultProps;
 
-	const { id, vNode } = prepareData(input, defaultProps, defaultOptions, waku);
+	const wrappedOptions = 'component' in input
+		? input
+		: { component: input };
+
+	const opt = defu({}, { ...defaultOptions, ...wrappedOptions }) as any;
+
+	const id: string = generateID();
+	const component = opt.component as DefineComponent;
+
+	const data = readonly(
+		mergeProps(defaultProps(id), {
+			...opt.props ?? {},
+			...opt?.emits ?? {}
+		})
+	);
+
+	component.inheritAttrs = opt.inheritAttrs;
+	const slots = opt.slots ? handleSlots(opt.slots) : undefined;
+	let vNode = createVNode(component, data, slots);
+
+	if (opt.target) {
+		const teleporter = h(Teleport as any, { to: opt.target });
+		vNode = h(teleporter, vNode);
+	}
+
+	vNode.appContext = waku?.instance?._context;
 
 	waku.addItem({
 		id,
