072ba879a9fdd4518506aade988486ea77da61e1
Subwaytime <leon.l@nophase.de>
2025-05-12 18:23:22 +0200

    add combined export

---
+++ docs/snippets/index.ts
@@ -0,0 +1,8 @@
+// export { removeComponent }  from "./removeComponent.ts";
+export { addComponentWithTeleport }  from "./addComponentWithTeleport.ts";
+export { addComponentWithReactiveProps }  from "./addComponentWithReactiveProps.ts";
+export { addComponentWithProps }  from "./addComponentWithProps.ts";
+export { addComponentWithHeaderSlot }  from "./addComponentWithHeaderSlot.ts";
+export { addComponentWithEmits }  from "./addComponentWithEmits.ts";
+export { addComponentWithDefaultSlot }  from "./addComponentWithDefaultSlot.ts";
+export { addComponent }  from "./addComponent.ts";
