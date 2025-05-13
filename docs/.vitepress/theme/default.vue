3febf150ee811ea32a0bd38913cf83543e4d6033
Subwaytime <leon.l@nophase.de>
2025-05-13 15:39:08 +0200

    remove div tag from default theme

--- docs/.vitepress/theme/default.vue
+++ docs/.vitepress/theme/default.vue
@@ -1,10 +1,8 @@
 <template>
   <Layout />
-  <div class="notifications" />
 </template>
 
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme';
-
 const { Layout } = DefaultTheme;
 </script>
