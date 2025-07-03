3e44bb6f2631f5c02b5bfb8a196f8913d5ee9594
Subwaytime <leon.l@nophase.de>
2025-07-04 00:42:02 +0200

    add WakuMointPoint export to index

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,5 @@ export {
 } from '~/actions/createSlot';
 
 export type { WakuData, WakuSlot } from '~/types';
+export { default as WakuMointPoint } from './mountPoint.vue';
 export { createWaku } from '~/createWaku';
